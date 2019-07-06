be2bbb433c54383113d4cfcf773f914aeff31d05
dizline <52178204+dizline@users.noreply.github.com>
2019-07-06 19:24:15 +0700

    Add files via upload

---
+++ rdiz.sh
@@ -0,0 +1,258 @@
+#!/system/bin/sh
+clear
+#perulangan
+ulang='y'
+while [ $ulang = 'y' ];
+do
+python2 password.py
+clear
+echo $me"starting tools... /";
+sleep 0.5;
+clear
+echo $ku'Starting tools... -';
+sleep 0.5;
+clear
+echo $i'sTarting tools... \';
+sleep 0.5;
+clear
+echo $me"stArting tools... /";
+sleep 0.5;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";                                                                                           sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "SABAR MEMEK -";
+sleep 0.4;
+clear
+echo 'SABAR NJING!... \';
+sleep 1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear                                                            
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+echo $me"                 ██████    ██    ██"
+echo $me"                 ██  ██████████████"
+echo $pu"                 ████████  ████  ██"
+echo $pu"                 ██  ██████████████"
+echo $bi"[X]"$i "X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•"$bi"[X]"
+echo $pu" |" $me"["$pu" Author :"$me"]"$bi" DimasPnr"$pu"                            |"
+echo $pu" |" $me"["$pu" WhatsApp :"$me"]"$bi" 083873098030"$pu"                      |"
+echo $pu" |" $me"["$pu" THANKS TO :"$me"]"$bi"[Allah Swt] [Deray] [Faiz]"$pu"        |"
+echo $bi"[X]"$i "X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•X•"$bi"[X]"
+echo
+echo $pu"•"$bi"────────────────────"$pu"•"
+echo $i"|"$cy"   TERMUX TOOLS"  $i"    |"
+echo $pu"•"$bi"────────────────────"$pu"•"
+echo $me"|"$pu"1"$me"|"$cy"["$cy"DarkFly""]"
+echo $me"|"$pu"2"$me"|"$cy"["$cy"DarkFBv6""]"
+echo $me"|"$pu"3"$me"|"$cy"["$cy"DarkFBv7""]"
+echo $me"|"$pu"4"$me"|"$cy"["$cy"VIRTEX""]"
+echo $me"|"$pu"5"$me"|"$cy"["$cy"Hack Facebook Massal""]"
+echo $me"|"$pu"6"$me"|"$cy"["$cy"WhoAreYou""]"$cy"("$pu"LACAK LOKASI DENGAN LINK"$cy")"
+echo $me"|"$pu"7"$me"|"$cy"["$cy"BAJINGANv6""]"
+echo $me"|"$pu"8"$me"|"$cy"["$cy"ShellPhish""]"$cy"("$pu"MEMBUAT PHISING"$CY")"
+echo $me"|"$pu"9"$me"|"$cy"["$cy"STARKV2""]"
+echo $me"|"$pu"10"$me"|"$cy"["$cy"INSTALL BAHAN BAHAN NJING""]"
+echo $me"|"$pu"0"$me"|"$cy"["$cy"KELUAR""]"
+echo $bi"["$cy"R.Diz"$bi"]"$bi"["$cy"Pilih Nomornya"$bi"]"
+echo $cy"╔"
+read -p"╚══>>" pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "N G O P I"|lolcat
+sleep 1
+git clone https://github.com/Ranginang67/DarkFly-Tool
+cd DarkFly-Tool
+python2 install.py
+DarkFly
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "N G O P I"|lolcat
+sleep 1
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+python2 SETAN.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "N G O P I"|lolcat
+sleep 1
+git clone https://github.com/LOoLzeC/reverse_engineering
+cd reverse_engineering
+cd darkontol-v7
+python2 darkontol-v7.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "N G O P I"|lolcat
+sleep 1
+git clone https://github.com/muhammadfathul/VIRTEX
+cd VIRTEX
+sh virtex.sh
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "N G O P I"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "N G O P I"|lolcat
+sleep 1
+git clone https://github.com/FajarTheGGman/WhoAreYou
+cd WhoAreYou
+sh install.sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "N G O P I"|lolcat
+sleep 1
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+cd BAJINGANv6
+sh BAJINGAN.sh
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "N G O P I"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/shellphish
+cd shellphish
+sh shellphish.sh
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "N G O P I"|lolcat
+sleep 1
+git clone https://github.com/aniketstark/STARK2.0
+cd STARK2.0
+python2 stark.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant "TUH GOBLOG"|lolcat
+fi
+
+if [ $pil = 0 ]
+then
+echo $me"TERIMA KASIH TELAH MENGGUNAKAN TOOL INI"
+sleep 1
+echo $me"Author:R.Diz"
+exit
+fi
+
+      echo 'ERROR : Wrong Input....! '
+      sleep 1
+      echo $ulang
+done
+    
